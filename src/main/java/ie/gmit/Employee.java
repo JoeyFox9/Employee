382d398b935187f4c65ba54fac04787e929ea68f
joeyf <g00361752@gmit.ie>
2021-03-10 09:34:29 +0000

    Functionality for Employee contructor complete

--- src/main/java/ie/gmit/Employee.java
+++ src/main/java/ie/gmit/Employee.java
@@ -1,4 +1,32 @@
 package ie.gmit;
 
 public class Employee {
+
+    private String name;
+    private long phone;
+    private long employeeNum;
+    private int age;
+
+    public Employee(String name, long phone, long employeeNum, int age){
+
+        if((name.length()>3) && (name.length() < 25)){
+            this.name = name;
+        }
+        else {
+            throw new IllegalArgumentException("invalid name error");
+        }
+
+        if (String.valueOf(phone).length() >= 8){
+            this.phone = phone;
+        }
+        else{ throw new IllegalArgumentException("invalid phone number error");}
+
+        this.employeeNum = employeeNum;
+
+        if(age > 16) {
+            this.age = age;
+        }
+        else{ throw new IllegalArgumentException("Age must be over 16");}
+
+    }
 }
--- src/main/java/ie/gmit/EmployeeList.java
+++ src/main/java/ie/gmit/EmployeeList.java
@@ -1,4 +1,11 @@
 package ie.gmit;
 
 public class EmployeeList {
+
+
+    public EmployeeList(String name, Long phone, Long employeeNum, int age){
+
+    }
+
+
 }
